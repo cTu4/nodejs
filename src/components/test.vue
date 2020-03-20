87d64121594b0681503da9a0a4babdf6a8374fda
cTu4 <chudik9898@mail.ru>
2020-03-20 16:48:22 +0300

    first

---
+++ src/components/test.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+    <li v-for="item in rows"><button>{{item}}</button></li>
+  </ul>
+</template>
+
+<script>
+    export default {
+        name: "test",
+        data(){
+           return  {rows: ['one','two']}
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,43 @@
+import Vue from 'vue'
+import App from './App.vue'
+import main from './components/test.vue'
+
+Ext.onReady(function () {
+  Ext.create({
+    xtype: 'panel',
+    title: 'APP VUE',
+    width: '100%',
+    height: window.innerHeight-100,
+    layout: 'border',
+    renderTo: Ext.getElementById('app'),
+    defaults: {
+      collapsible: true,
+      split: true,
+      bodyPadding: 15
+    },
+    items: [
+      {
+        title: 'Left',
+        region: 'west',
+        minWidth: 200
+      },
+      {
+        xtype: 'panel',
+        title: "Center",
+        region: 'center',
+        collapsible: false,
+        html: '<div id="vue"></div>',
+      }
+    ],
+    listeners: {
+      afterRender(){
+        new Vue({
+          el: '#vue',
+          render: h => h(main)
+        });
+      }
+    }
+
+  });
+});
+
